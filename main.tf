e0bf527745fce485c7ebf35516137fd9ad6ca18e
BrunoReboul <brunoreboul@google.com>
2021-10-28 08:54:31 +0200

    rm project feeds

--- main.tf
+++ main.tf
@@ -35,14 +35,12 @@ module "fetchrules" {
 }
 
 module "setfeed" {
-  source                   = "./modules/setfeed"
-  project_id               = var.project_id
-  cai_feed_topic_name      = var.cai_feed_topic_name
-  pubsub_allowed_regions   = var.pubsub_allowed_regions
-  feed_iam_policy_folders  = var.feed_iam_policy_folders
-  feed_iam_policy_orgs     = var.feed_iam_policy_orgs
-  feed_iam_policy_projects = var.feed_iam_policy_projects
-  feed_resource_folders    = var.feed_resource_folders
-  feed_resource_orgs       = var.feed_resource_orgs
-  feed_resource_projects   = var.feed_resource_projects
+  source                  = "./modules/setfeed"
+  project_id              = var.project_id
+  cai_feed_topic_name     = var.cai_feed_topic_name
+  pubsub_allowed_regions  = var.pubsub_allowed_regions
+  feed_iam_policy_folders = var.feed_iam_policy_folders
+  feed_iam_policy_orgs    = var.feed_iam_policy_orgs
+  feed_resource_folders   = var.feed_resource_folders
+  feed_resource_orgs      = var.feed_resource_orgs
 }
