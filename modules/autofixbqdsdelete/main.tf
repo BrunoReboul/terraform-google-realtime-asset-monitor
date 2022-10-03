20744ac0611e787bc851aac8d5d763fa543d0b22
Bruno REBOUL <BrunoReboul@users.noreply.github.com>
2022-10-03 15:55:49 +0200

    update filter with origin real-time (#46)

--- modules/autofixbqdsdelete/main.tf
+++ modules/autofixbqdsdelete/main.tf
@@ -143,7 +143,7 @@ resource "google_pubsub_subscription" "subcription" {
   expiration_policy {
     ttl = ""
   }
-  filter                     = "attributes.ce-type = \"com.gitlab.realtime-asset-monitor.violation_on_rule.${local.rule_name}\""
+  filter                     = "attributes.ce-type = \"com.gitlab.realtime-asset-monitor.violation_on_rule.${local.rule_name}.real-time\""
   message_retention_duration = var.sub_message_retention_duration
   retry_policy {
     minimum_backoff = var.sub_minimum_backoff
