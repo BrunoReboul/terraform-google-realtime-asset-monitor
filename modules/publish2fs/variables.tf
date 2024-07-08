9f77f0b381df8c600bffcba502e7fcad1b826893
bruno reboul <brunoreboul@google.com>
2024-07-08 13:25:27 +0200

    chore(publish2fs):mig to crun v2 resource

--- modules/publish2fs/variables.tf
+++ modules/publish2fs/variables.tf
@@ -47,9 +47,9 @@ variable "crun_memory" {
 }
 
 
-variable "crun_timeout_seconds" {
+variable "crun_timeout" {
   description = "Max duration for an instance for responding to a request"
-  default     = 180
+  default     = "180s"
 }
 
 variable "ram_container_images_registry" {
