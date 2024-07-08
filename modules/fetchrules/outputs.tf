35481a9d37b05912d5fafca55644f22047d4ba96
bruno reboul <brunoreboul@google.com>
2024-07-08 11:46:18 +0200

    chore(fetchrules):mig to crun v2 resource

--- modules/fetchrules/outputs.tf
+++ modules/fetchrules/outputs.tf
@@ -31,12 +31,13 @@ output "rules_repo_bucket_name" {
 
 output "crun_service_id" {
   description = "cloud run service id"
-  value       = google_cloud_run_service.crun_svc.id
+  value       = google_cloud_run_v2_service.crun_svc.id
 }
 output "crun_service_url" {
   description = "cloud run service url"
-  value       = google_cloud_run_service.crun_svc.status[0].url
+  value       = google_cloud_run_v2_service.crun_svc.uri
 }
+
 output "subscription_sa_email" {
   description = "Service account email used to trigger this type of action"
   value       = google_service_account.subscription_sa.email
