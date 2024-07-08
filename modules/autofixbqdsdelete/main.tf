43a86e4c8a9e081d2991d72873fb70dc6c5f2929
bruno reboul <brunoreboul@google.com>
2024-07-08 08:28:32 +0200

    chore(autofixbqdsdelete):migrate to crun v2 resource

--- modules/autofixbqdsdelete/main.tf
+++ modules/autofixbqdsdelete/main.tf
@@ -37,16 +37,21 @@ resource "google_project_iam_member" "project_profiler_agent" {
   member  = "serviceAccount:${google_service_account.microservice_sa.email}"
 }
 
-resource "google_cloud_run_service" "crun_svc" {
+resource "google_cloud_run_v2_service" "crun_svc" {
   project  = var.project_id
   name     = "${local.service_name}${local.action_kind}"
   location = var.crun_region
-
+  client = "terraform"
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+  traffic {
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
   template {
-    spec {
-      containers {
+    containers {
         image = "${var.ram_container_images_registry}/${local.service_name}:${var.ram_microservice_image_tag}"
         resources {
+          cpu_idle = true
           limits = {
             cpu    = "${var.crun_cpu}"
             memory = "${var.crun_memory}"
@@ -72,27 +77,13 @@ resource "google_cloud_run_service" "crun_svc" {
           name  = "${upper(local.service_name)}_ACTION_KIND"
           value = local.action_kind
         }
-      }
-      container_concurrency = var.crun_concurrency
-      timeout_seconds       = var.crun_timeout_seconds
-      service_account_name  = google_service_account.microservice_sa.email
     }
-    metadata {
-      annotations = {
-        "run.googleapis.com/client-name"   = "terraform"
-        "autoscaling.knative.dev/maxScale" = "${var.crun_max_instances}"
-      }
+    max_instance_request_concurrency = var.crun_concurrency
+    timeout = var.crun_timeout
+    service_account = google_service_account.microservice_sa.email
+    scaling {
+      max_instance_count = var.crun_max_instances
     }
-  }
-  metadata {
-    annotations = {
-      "run.googleapis.com/ingress" = "internal"
-    }
-  }
-  autogenerate_revision_name = true
-  traffic {
-    percent         = 100
-    latest_revision = true
   }
   lifecycle {
     ignore_changes = all
@@ -114,9 +105,9 @@ data "google_iam_policy" "binding" {
   }
 }
 resource "google_cloud_run_service_iam_policy" "trigger_invoker" {
-  location = google_cloud_run_service.crun_svc.location
-  project  = google_cloud_run_service.crun_svc.project
-  service  = google_cloud_run_service.crun_svc.name
+  location = google_cloud_run_v2_service.crun_svc.location
+  project  = google_cloud_run_v2_service.crun_svc.project
+  service  = google_cloud_run_v2_service.crun_svc.name
 
   policy_data = data.google_iam_policy.binding.policy_data
 }
@@ -131,7 +122,7 @@ resource "google_pubsub_subscription" "subcription" {
       service_account_email = google_service_account.subscription_sa.email
     }
     #Updated endpoint to deal with WARNING in logs: failed to extract Pub/Sub topic name from the URL request path: "/", configure your subscription's push endpoint to use the following path pattern: 'projects/PROJECT_NAME/topics/TOPIC_NAME
-    push_endpoint = "${google_cloud_run_service.crun_svc.status[0].url}/${var.triggering_topic_id} "
+    push_endpoint = google_cloud_run_v2_service.crun_svc.uri
   }
   expiration_policy {
     ttl = ""
